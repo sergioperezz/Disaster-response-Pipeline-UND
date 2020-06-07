e9ae47fd984b8441b0c8d71fb8933807a596f70c
Consultant <Consultant@IKL000420.ikoscorp.key>
2020-06-07 21:18:09 +0200

    process_data with replacing db

--- data/process_data.py
+++ data/process_data.py
@@ -53,7 +53,7 @@ def save_data(df, database_filename):
     return: 
     """
     engine = create_engine('sqlite:///'+ database_filename)
-    df.to_sql('df', engine, index=False)
+    df.to_sql('df', engine, index=False,if_exists='replace')
 
 
 def main():
